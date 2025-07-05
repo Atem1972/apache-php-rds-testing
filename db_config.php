c9d56d342f82fb2e1959d558c668efb10417ab2f
Atem1972 <154833460+Atem1972@users.noreply.github.com>
2025-07-05 22:55:48 +0200

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,10 +1,10 @@
 <?php
-$host = "DB-ENDPOINT";
-$username = "admin";
-$password = "yourpassword";
-$database = "utrains";
+$host = "${db_host}";
+$username = "${db_name}";
+$password = "${db_user}";
+$dbname =  "${db_password}";                #"utrains";
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
