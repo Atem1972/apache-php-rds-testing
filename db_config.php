d5c37141b869b2cba7db08abb3fbcef5f4b7a7f5
Atem1972 <154833460+Atem1972@users.noreply.github.com>
2025-07-06 00:35:34 +0200

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,13 +1,2 @@
-<?php
-$host = "${db_host}";
-$username = "${db_user}";
-$password = "${db_password}";
-$dbname = "${db_name}";
 
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
 
